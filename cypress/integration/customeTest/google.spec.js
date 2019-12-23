48350ab3266d63721d59f5a5c06cd1787f127ae3
sara <sarangan12@gmail.com>
2019-12-24 03:17:19 +0800

    test

--- cypress/integration/customeTest/google.spec.js
+++ cypress/integration/customeTest/google.spec.js
@@ -5,10 +5,49 @@ describe('Test google', () => {
         cy.contains('Gmail').click();
         cy.url().should('include', 'mail');
 
+        // cy.pause();
+
         cy.go('back');
         cy.wait(3000);
-        
+
         cy.get('input[name=q]').type('cypress').should('have.value', 'cypress');
+
+
+        // cy.get('#main-content') ? jQuery
+        //     .find('.article')
+        //     .children('img[src^="/static"]')
+        //     .first()
+
+
+        // cy.exec('npm run db:reset && npm run db:seed') ?execute
+
+        ///cy.visit('/login')
+        // cy.get('input[name=username]').type(username)
+
+        // // {enter} causes the form to submit
+        // cy.get('input[name=password]').type(`${password}{enter}`)
+
+
+        ///
+        // cy.request('POST', '/login', { //POST MAN
+        //     username,
+        //     password
+        //   })
+      
+        //   // now that we're logged in, we can visit
+        //   // any kind of restricted route!
+        //   cy.visit('/dashboard')
+
+        // // we should be redirected to /dashboard
+        // cy.url().should('include', '/dashboard')
+
+        // // our auth cookie should be present
+        // cy.getCookie('your-session-cookie').should('exist')
+
+        // // UI should reflect this user being logged in
+        // cy.get('h1').should('contain', 'jane.lane')
+
+
     });
   
 });
---
+++ cypress/integration/customeTest/w3school.spec.js
@@ -0,0 +1,69 @@
+describe('W3 School', () => {
+  it('test form', ()=>{
+    // cy.visit('https://www.w3schools.com/html');
+    // cy.get('#leftmenuinnerinner', { timeout: 300 }).scrollTo('bottom');
+    // cy.pause();
+    // cy.get('#leftmenuinnerinner a', { timeout: 300 }).contains('HTML Forms').click();
+
+    // cy.contains('a[href$="html_forms.asp"]').click();
+    // cy.wait(100);
+    cy.visit('https://www.w3schools.com/html/html_forms.asp');
+    cy.get('input[name=firstname]').clear().type('neck');
+    cy.get('input[name=lastname]').clear().type('shoulder');
+    cy.get('Submit').click();
+    cy.url().should('include', 'tryhtml_form_submit');
+    cy.get('input[name=firstname]').should('have.value', 'neck');
+
+
+//     cy.get('#element')
+//   .then(($myElement) => {
+//     doSomething($myElement)
+//   })
+
+
+cy.get(':checkbox').should('be.disabled')
+cy.get('form').should('have.class', 'form-horizontal')
+cy.get('input').should('not.have.value', 'US')
+cy.get('button.close').click().should('not.exist')
+cy.get('#header a').should('have.class', 'active').and('have.attr', 'href', '/users')
+cy.get('.mobile-nav').should('be.visible').and('contain', 'Home')
+cy.get('.article').find('footer')
+cy.get('#parent').find('li')
+
+
+cy
+  .get('.my-selector')
+  .as('myElement') // sets the alias
+  .click()
+
+cy
+  .get('@myElement') // re-queries the DOM as before (only if necessary)
+  .click()
+
+  cy.request('/users/1').its('body').should('deep.eq', { name: 'Jane' })
+
+
+
+
+
+  });
+});
+
+
+describe('if your app uses jQuery', function () {
+    ['mouseover', 'mouseout', 'mouseenter', 'mouseleave'].forEach((event) => {
+      it('triggers event: ' + event, function () {
+        // if your app uses jQuery, then we can trigger a jQuery
+        // event that causes the event callback to fire
+        cy
+          .get('#with-jquery').invoke('trigger', event)
+          .get('#messages').should('contain', 'the event ' + event + 'was fired')
+      })
+    })
+  })
+
+  it.skip('returns "fizz" when number is multiple of 3', function () {
+    numsExpectedToEq([9, 12, 18], 'fizz')
+  })
+
+
